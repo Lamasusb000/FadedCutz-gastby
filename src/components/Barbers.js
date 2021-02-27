721237c9dee237ff365b5d650cc50b9c0677bef9
Lamasusb000 <Usbaldolamasjr@gmail.com>
2021-02-26 22:01:50 -0800

    First Attempt

---
+++ src/components/Barbers.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../CSS/Barbers.css"
+
+const Barbers = () => (
+    <div
+        className="Barbers-Container"
+    >
+        Test Text
+    </div>
+)
+
+export default Barbers
--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,13 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import "../CSS/Header.css"
+
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+    className="Header"
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+    <div>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
