a6974a20aa67be3165fc5052c810708c08ae6cea
Lamasusb000 <Usbaldolamasjr@gmail.com>
2021-02-26 22:04:30 -0800

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -69,7 +69,11 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <script
+      src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+      ></script>
+    </Helmet>
   )
 }
 
